8b265e97323fd9879676e8efbe6e11cb2063a42a
luoguifeng <luoguifeng14@126.com>
2019-08-09 14:36:36 +0800

    case1 2019.8.9

--- case1/studentsystem.py
+++ case1/studentsystem.py
@@ -178,7 +178,37 @@ def modify():
 
 # 学生成绩排名
 def sort():
-    pass
+    show()
+    if os.path.exists(filename):
+        with open(filename, 'r') as file:
+            student_old = file.readlines()
+            student_new = []
+        for list in student_old:
+            d = dict(eval(list))
+            student_new.append(d)
+    else:
+        return
+    ascORdesc = input('请选择排序方式（0升序；1降序）：')
+    if ascORdesc == '0':
+        ascORdescBool = False
+    elif ascORdesc == '1':
+        ascORdescBool = True
+    else:
+        print('您的输入有误，请重新输入！')
+        sort()
+    mode = input('请选择排序方式（1按英语成绩排序；2按python成绩排序；3按C语言成绩排序；0按总成绩排序）：')
+    if mode == '1':
+        student_new.sort(key=lambda x:x['english'], reverse=ascORdescBool)
+    elif mode == '2':
+        student_new.sort(key=lambda x:x['python'], reverse=ascORdescBool)
+    elif mode == '3':
+        student_new.sort(key=lambda x:x['c'], reverse=ascORdescBool)
+    elif mode == '0':
+        student_new.sort(key=lambda x: x['english'] + x['python'] + x['c'], reverse=ascORdescBool)
+    else:
+        print('您的输入有误，请重新输入！')
+        sort()
+    show_student(student_new)
 
 
 # 统计学生总人数
@@ -196,7 +226,16 @@ def total():
 
 # 显示所有学生信息
 def show():
-    pass
+    student_new = []
+    if os.path.exists(filename):
+        with open(filename, 'r') as rfile:
+            student_old = rfile.readlines()
+            for list in student_old:
+                student_new.append(eval(list))
+            if student_new:
+                show_student(student_new)
+    else:
+        print('暂未保存数据信息...')
 
 
 # 将学生信息保存到文件
