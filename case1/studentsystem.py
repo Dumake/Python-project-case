6c6c177cdb98f20de5d8ac7484b6f541b984144b
luoguifeng <luoguifeng14@126.com>
2019-08-09 19:01:17 +0800

    case1 2019.8.9

--- case1/studentsystem.py
+++ case1/studentsystem.py
@@ -19,16 +19,16 @@ def main():
         option_str = re.sub("\D", "", option)
         if option_str in ['0', '1', '2', '3', '4', '5', '6', '7']:
             option_int = int(option_str)
-            if option_int == 0 :
+            if option_int == 0:
                 print("您已经退出学生信息管理系统！")
                 ctrl = False
             elif option_int == 1:
                 insert()
-            elif option_int ==2:
+            elif option_int == 2:
                 search()
-            elif option_int ==3:
+            elif option_int == 3:
                 delete()
-            elif option_int ==4:
+            elif option_int == 4:
                 modify()
             elif option_int == 5:
                 sort()
@@ -261,23 +261,24 @@ def show_student(studentlist):
         print(format_data.format(info.get('id'), info.get('name'), str(info.get('english')), str(info.get('python')),
                                  str(info.get('c')), str(info.get('english') + info.get('python') + info.get('c')).center(12)))
 
+
 def menu():
     print("""    
-    |————————————学生信息管理系统————————————|
-    |                                      |
-    |   ============功能菜单============    |
-    |                                       |
-    |   1.录入学生成绩信息                   |
-    |   2.查找学生成绩信息                   |
-    |   3.删除学生信息                       |
-    |   4.修改学生成绩信息                   |
-    |   5.学生成绩排名                       |
-    |   6.统计学生总人数                     |
-    |   7.显示所有学生信息                   |
-    |   0.退出系统                          |
-    |  ==================================  |
-    |  说明：通过数字或↑↓方向键选择菜单       |
-    |———————————————————————————————————————|
+    ————————————学生信息管理系统———————————— 
+                                         
+       ============功能菜单============     
+                                           
+       1.录入学生成绩信息                   
+       2.查找学生成绩信息                   
+       3.删除学生信息                       
+       4.修改学生成绩信息                   
+       5.学生成绩排名                       
+       6.统计学生总人数                     
+       7.显示所有学生信息                   
+       0.退出系统                          
+      ================================  
+      说明：通过数字或↑↓方向键选择菜单       
+    —————————————————————————————————————
         """)
 
 
