6b6e2144b9ceb7e9633b03c12139699320936682
luoguifeng <luoguifeng14@126.com>
2019-08-14 11:51:13 +0800

    create case17

---
+++ case17/app/admin/forms.py
@@ -0,0 +1,9 @@
+# 本程序所使用的语言版本为:Python3.7
+# -*-encoding: utf-8 -*-
+# @Author    : Luo Guifeng
+# @Email     : luoguifeng14@126.com
+# @IDE       : PyCharm
+# @Project   : Python-project-case
+# @File      : forms.py
+# @Date      : 2019/8/14
+# @Time      : 11:49
