745c88a06d225f826aedb36d5223e05662b7fad6
luoguifeng <luoguifeng14@126.com>
2019-08-15 14:21:16 +0800

    update case17 2019.08.15

--- case17/app/models.py
+++ case17/app/models.py
@@ -14,10 +14,10 @@ from datetime import datetime
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Srting(100))
-    password = db.Column(db.Srting(100))
-    email = db.Column(db.Srting(100), unique=True)
-    phone = db.Column(db.Srting(11), unique=True)
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    phone = db.Column(db.String(11), unique=True)
     consumption = db.Column(db.DECIMAL(10, 2), default=0)
     addtime = db.Column(db.DateTime, index=True, default=datetime.now)
     orders = db.relationship('Orders', backref='user')
@@ -33,8 +33,8 @@ class User(db.Model):
 class Admin(db.Model):
     __tablename__ = 'admin'
     id = db.Column(db.Integer, primary_key=True)
-    manager = db.Column(db.Srting(100), unique=True)
-    password = db.Column(db.Srting(100))
+    manager = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
 
     def __repr__(self):
         return '<Admin {}>'.format(self.manager)
@@ -47,12 +47,39 @@ class Admin(db.Model):
 class SuperCat(db.Model):
     __tablename__ = 'supercat'
     id = db.Column(db.Integer, primary_key=True)
-    cat_name = db.Column(db.Srting(100))
+    cat_name = db.Column(db.String(100))
     addtime = db.Column(db.DateTime, index=True, default=datetime.now)
     subcat = db.relationship('Subcat', backref='supercat')
     goods = db.relationship('Goods', backref='supercat')
 
     def __repr__(self):
         return '<SuperCat {}>'.format(self.cat_name)
+
+
+class SubCat(db.Model):
+    __tablename__ = 'subcat'
+    id = db.Column(db.Integer, primary_key=True)
+    cat_name = db.Column(db.String(100))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
+    super_cat_id = db.Column(db.Integer, db.ForeignKey('supercat.id'))
+    goods = db.relationship('Goods', backref='subcat')
+
+    def __repr__(self):
+        return '<SubCat {}>'.format(self.cat_name)
+
+
+class Goods(db.Model):
+    __tablename__ = 'goods'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    original_price = db.Column(db.DECIMAL(10, 2))
+    current_price = db.Column(db.DECIMAL(10, 2))
+    picture = db.Column(db.String(255))
+    introduction = db.Column(db.Text)
+    views_count = db.Column(db.Integer, default=0)
+    is_sale = db.Column(db.Boolean(), default=0)
+    is_new = db.Column(db.Boolean(), default=0)
+
+    supercat_id = db.Column(db.Integer, db.ForeignKey('supercat.id'))
 
 
