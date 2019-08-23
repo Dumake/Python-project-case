ff532d8048ed07f83fd9cd175b3c0ae0978e86a4
luoguifeng <luoguifeng14@126.com>
2019-08-23 12:13:13 +0800

    update case17 2019.08.23

--- case17/app/home/views.py
+++ case17/app/home/views.py
@@ -7,3 +7,18 @@
 # @File      : views.py
 # @Date      : 2019/8/14
 # @Time      : 11:50
+
+from . import home
+from app.home.forms import RegisterForm
+from flask import render_template
+
+
+@home.route("/register/", methods=['GET', 'POST'])
+def register():
+    """
+    登录
+    :return:
+    """
+    form = RegisterForm()
+    # ...
+    return render_template('home/register.html', form=form)
